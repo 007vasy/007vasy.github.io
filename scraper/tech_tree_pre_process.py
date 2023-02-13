993da13350a088d169c7a547aedc2c3ab82b4bb5
ben <12837887+007vasy@users.noreply.github.com>
2023-02-13 11:51:56 +1100

    tech tree first cut

--- scraper/tech_tree_pre_process.py
+++ scraper/tech_tree_pre_process.py
@@ -5,6 +5,27 @@ import json
 
 TECH_TREE_FOLDER = Path(__file__).parent /"tech_tree_raw_data"
 
+def convert_color_hash_to_rgb(color_hash, subtree):
+    color_palette = {
+        'spacetree': 'rgb(50, 205, 50)',
+        'nanotech': 'rgb(30, 144, 255)',
+        'intcoop': 'rgb(255, 165, 0)',
+        'neurotech': 'rgb(255, 192, 203)',
+        'longevity': 'rgb(107, 142, 35)'
+    }
+
+    if subtree is not None:
+        rgb_color = color_palette.get(subtree, 'rgb(255,0,0)')
+
+    if color_hash is not None:
+        # color_hash is in format #RRGGBB
+        r = int(color_hash[1:3], 16)
+        g = int(color_hash[3:5], 16)
+        b = int(color_hash[5:7], 16)
+        rgb_color = f'rgb({r},{g},{b})'
+
+    return rgb_color
+
 def main():
 
     processed_data = {
@@ -34,7 +55,7 @@ def main():
                     processed_data['nodes'].append({
                         'id':new_id,
                         'label':item['label'],
-                        'fill':item.get('fill','red'),
+                        'fill':convert_color_hash_to_rgb(item.get('fill'),short_name),
                         'desc':item['desc'],
                         'tags':item['tags'],
                         'subtree':short_name
@@ -69,6 +90,20 @@ def main():
             offset = max_id+1
         else:
             print(f'Raw data is None: {file}')
+    
+    degree_info = {}
+
+    for edge in processed_data['links']:
+        if edge['source'] not in degree_info:
+            degree_info[edge['source']] = 0
+        if edge['target'] not in degree_info:
+            degree_info[edge['target']] = 0
+
+        degree_info[edge['source']] += 1
+        degree_info[edge['target']] += 1
+
+    for node in processed_data['nodes']:
+        node['degree'] = degree_info.get(node['id'], 0)
 
     save_json(processed_data, Path(__file__).parent/'tech_tree.json')        
 
