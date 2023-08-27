1ec3163eaf7ecda8832641f64155ba96961d6bb0
ben <12837887+007vasy@users.noreply.github.com>
2023-08-27 13:51:02 +1000

    add converter

--- scraper/cosmo_csv_to_json.py
+++ scraper/cosmo_csv_to_json.py
@@ -3,9 +3,10 @@ FILE = "cosmo_test.csv"
 
 import csv
 import json
+from tqdm import tqdm
 
 # CSV schema:
-# source,target,size,time
+# target,target,size,time
 # cryptojamie7,0xRacerAlt,4,1691704263000
 
 # JSON schema:
@@ -13,31 +14,72 @@ import json
 #   "nodes": [
 #     {
 #       "id": 0,
-#       "url": "https://brilliant.org/courses/pre-algebra/",
-#       "name": "Solving Equations",
-#       "type": "Algebra",
-#       "inDegree": 1,
-#       "outDegree": 2
+#       "name": "Banteg",
+#       "type": "User",
 #     },
 #   ]
 #   "links": [
 #     {
 #       "source": 0,
 #       "target": 11,
-#       "type": "NextStep",
-#       "curvature": -0.2,
-#       "rotation": -0.2
+#       "type": "Follows",
+#       "time": 1691704263000,
 #     },
 #   ]
 # }
+def add_user_to_force_3d_graph_json(user, force_3d_graph):
+
+    force_3d_graph['nodes'].append({
+        "id": user,
+        "name": user,
+        "type": "User",
+    })
+
+    return force_3d_graph
 
 def cosmo_csv_to_force_3d_graph_json(file):
+
+    force_3d_graph = {
+        "nodes": [],
+        "links": []
+    }
+
+    users = set()
 
     with open(file, 'r') as f:
         reader = csv.reader(f)
         
-        for row in next(reader):
-            print(row)
+        for i, row in enumerate(tqdm(reader)):
+            if i == 0:
+                continue
+
+            source = row[0]
+            target = row[1]
+            time = row[3]
+
+            if source == target:
+                continue
+
+            if source not in users:
+                users.add(source)
+                force_3d_graph = add_user_to_force_3d_graph_json(source, force_3d_graph)
+            
+            if target not in users:
+                users.add(target)
+                force_3d_graph = add_user_to_force_3d_graph_json(target, force_3d_graph)
+
+            # Add link
+            force_3d_graph['links'].append({
+                "source": source,
+                "target": target,
+                "type": "Follows",
+                "time": time,
+            })
+
+    with open('cosmo_test.json', 'w') as f:
+        json.dump(force_3d_graph, f, indent=4)
+
+
 
 
 def main():
