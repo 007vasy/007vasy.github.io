5e2c84d0260d8587d3295e87ec8a4016a0e7c89d
benf <12837887+007vasy@users.noreply.github.com>
2023-10-06 13:02:38 +1100

    add ccip scrape

--- scraper/ccip_scraper.py
+++ scraper/ccip_scraper.py
@@ -6,9 +6,31 @@ def extract_ccip_info(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     data = {"nodes": [], "links": []}
-    node_id = 1
+    raw_lanes = []
     for section in soup.find_all('section'):
-        print(section.prettify())
+        for a in section.find_all('a'):
+            url = a['href']
+            if "--" in url:
+                raw_lanes.append(url.replace("#","").replace("-lane","").split('--'))
+
+    # add nodes from raw_lanes
+    nodes = []
+    links = []
+
+    for lane_source, lane_target  in raw_lanes:
+        if lane_source not in nodes:
+            nodes.append(lane_source)
+        if lane_target not in nodes:
+            nodes.append(lane_target)
+
+    for id, node in enumerate(nodes):
+        data["nodes"].append({"id": id, "name": node, "url": f"https://docs.chain.link/ccip/supported-networks/#{node}"})
+
+    # add links from raw_lanes
+    for lane_source, lane_target in raw_lanes:
+        source = nodes.index(lane_source)
+        target = nodes.index(lane_target)
+        data["links"].append({"source": source, "target": target, "name": f"{lane_source} > {lane_target}", "url": f"https://docs.chain.link/ccip/supported-networks/#{lane_source}--{lane_target}-lane"})
 
     with open('ccip_info.json', 'w') as f:
         json.dump(data, f, indent=2)
