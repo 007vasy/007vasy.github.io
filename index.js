254539557912a10982d1c6951d5fd2477157ff83
benf <12837887+007vasy@users.noreply.github.com>
2024-02-27 17:32:50 +1100

    viz fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 65000;
 // const file_to_convert = process.env.FILE_TO_CONVERT || "";
 
 // if (file_to_convert != "") {
